b43080b58d28a444c248d50265239d51bec01c2c
jEx <jules.exbrayat@ensimag-inp.org>
2024-09-23 14:44:59 +0200

    make gradient descent

--- backpropagation.cpp
+++ backpropagation.cpp
@@ -26,7 +26,7 @@ make_gradient_descent(
 {
 
     // Define constants
-    int dataset_size = x_train.shape()[0]; 
+    int dataset_size = x_train.shape()[0];
 
     // Initialize network
 
@@ -75,7 +75,45 @@ make_gradient_descent(
 
             // Make backpropagation
 
-            xarray<double> delta3 = 
+            xarray<double> delta3 = (a3 - y_train(1, i)) * sigma_derivative(z3);
+            xarray<double> delta2 = linalg::dot(transpose(w3), delta3) * sigma_derivative(z2);
+            xarray<double> delta1 = linalg::dot(transpose(w2), delta2) * sigma_derivative(z1);
+
+            xarray<double> gradient_b1 = delta1;
+            xarray<double> gradient_b2 = delta2;
+            xarray<double> gradient_b3 = delta3;
+
+            xarray<double> gradient_w1 = zeros<double>(w1.shape());
+            xarray<double> gradient_w2 = zeros<double>(w2.shape());
+            xarray<double> gradient_w3 = zeros<double>(w3.shape());
+
+            for (int j = 0; j < w1.shape()[0]; j++) {
+                for (int k = 0; k < w1.shape()[1]; k++) {
+                    gradient_w1[j, k] = a0[k] * delta1[j];
+                }
+            }
+
+            for (int j = 0; j < w2.shape()[0]; j++) {
+                for (int k = 0; k < w2.shape()[1]; k++) {
+                    gradient_w2[j, k] = a1[k] * delta2[j];
+                }
+            }
+
+            for (int j = 0; j < w3.shape()[0]; j++) {
+                for (int k = 0; k < w3.shape()[1]; k++) {
+                    gradient_w3[j, k] = a2[k] * delta3[j];
+                }
+            }
+
+
+            // Updating biases and weights
+            b1 -= learning_rate * gradient_b1;
+            b2 -= learning_rate * gradient_b2; 
+            b3 -= learning_rate * gradient_b3; 
+            w1 -= learning_rate * gradient_w1;  
+            w2 -= learning_rate * gradient_w2;  
+            w3 -= learning_rate * gradient_w3;  
+
         }
     }
 

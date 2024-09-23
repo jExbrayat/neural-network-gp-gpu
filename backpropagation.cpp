df20d2665fea6902435b31796e07b8a5a34ed416
jEx <jules.exbrayat@ensimag-inp.org>
2024-09-23 14:02:14 +0200

    make forward pass

--- backpropagation.cpp
+++ backpropagation.cpp
@@ -5,6 +5,7 @@
 #include <xtensor/xrandom.hpp>
 #include <xtensor-blas/xlinalg.hpp>
 #include <tuple>
+#include <cmath>
 #include "utils.cpp"
 using namespace xt::placeholders; // to enable _ syntax
 using namespace std;
@@ -20,7 +21,7 @@ std::tuple<
 >
 make_gradient_descent(
     xarray<double> x_train,
-    xarray<double> y_train,
+    xarray<double> y_train, // shape must be (1, n)
     int epochs,
     float learning_rate
 ) {
@@ -45,6 +46,40 @@ make_gradient_descent(
     xarray<double> nabla_w1 = zeros<double>(w1.shape());
     xarray<double> nabla_w2 = zeros<double>(w2.shape());
     xarray<double> nabla_w3 = zeros<double>(w3.shape());
+
+    // Init predicted values array
+    xarray<double> y_pred = zeros<double>({1, dataset_size});
+
+    for (int epoch = 0; epoch < epochs; epoch++)
+    {
+        float mse = 0;
+        for (int i = 0; i < dataset_size; i++)
+        {
+
+            // Input layer
+            xarray<double> a0 = xt::view(x_train, i, xt::all());
+            a0 = a0.reshape({1, 2});
+
+            // First hidden layer
+            auto z1 = xt::linalg::dot(w1, a0);
+            auto a1 = sigma(z1);
+
+            // Second hidden layer
+            auto z2 = xt::linalg::dot(w2, a1) + b2;
+            auto a2 = sigma(z2);
+
+            // Third hidden layer
+            auto z3 = xt::linalg::dot(w3, a2) + b3;
+            auto a3 = sigma(z3); // prediction
+
+            // Compute MSE
+            mse += std::pow(a3(1, 1) - y_train(1, i), 2);
+
+            // Make backpropagation
+
+            xarray<double> delta3 = 
+        }
+    }
 
     return std::make_tuple(w1, w2, w3, b1, b2, b3);
 }

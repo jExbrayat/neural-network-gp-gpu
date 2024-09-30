a93e88d659fb098ccdf712e75aa0675ddca12e3d
jEx <jules.exbrayat@ensimag-inp.org>
2024-09-30 14:06:23 +0200

    remove useless code and format document

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -33,12 +33,12 @@ void gnuplot(xarray<double> two_dimensional_dataset, xarray<int> true_pred, stri
 
     // Use Gnuplot to plot the data
     std::ofstream gnuplot_commands(gnuplot_commands_path);
-    gnuplot_commands << "set terminal wxt size 1200,800\n";         // Set window size
-    gnuplot_commands << "set xlabel 'x1'\n";                        // Set x-axis label
-    gnuplot_commands << "set ylabel 'x2'\n";                        // Set y-axis label
-    gnuplot_commands << "set title '" + plot_title + "'\n"; // Set plot title
-    gnuplot_commands << "set pointsize 1.5\n";                        // Set point size
-    gnuplot_commands << "set palette defined (0 'blue', 1 'red')\n"; // Define color palette for clusters
+    gnuplot_commands << "set terminal wxt size 1200,800\n";                           // Set window size
+    gnuplot_commands << "set xlabel 'x1'\n";                                          // Set x-axis label
+    gnuplot_commands << "set ylabel 'x2'\n";                                          // Set y-axis label
+    gnuplot_commands << "set title '" + plot_title + "'\n";                           // Set plot title
+    gnuplot_commands << "set pointsize 1.5\n";                                        // Set point size
+    gnuplot_commands << "set palette defined (0 'blue', 1 'red')\n";                  // Define color palette for clusters
     gnuplot_commands << "plot 'temp/gnuplot.dat' u 1:2:3 with points palette pt 7\n"; // Plot data points using the third column for color
 
     // Close the file
@@ -48,7 +48,8 @@ void gnuplot(xarray<double> two_dimensional_dataset, xarray<int> true_pred, stri
     std::system(("unset GTK_PATH && gnuplot-x11 -persist " + gnuplot_commands_path).c_str());
 }
 
-void gnuplot_ypred_ytrue(const xt::xarray<double>& y_true, const xt::xarray<double>& y_pred, const std::string& plot_title) {
+void gnuplot_ypred_ytrue(const xt::xarray<double> &y_true, const xt::xarray<double> &y_pred, const std::string &plot_title)
+{
     // Define paths
     std::string gnuplot_commands_path = "temp/gnuplot_commands.gp";
     std::string data_path = "temp/gnuplot.dat";
@@ -58,18 +59,19 @@ void gnuplot_ypred_ytrue(const xt::xarray<double>& y_true, const xt::xarray<doub
 
     // Create a data file
     std::ofstream data(data_path);
-    for (int i = 0; i < n_rows; i++) {
+    for (int i = 0; i < n_rows; i++)
+    {
         data << y_true(i, 0) << " " << y_pred(i, 0) << "\n";
     }
     data.close();
 
     // Create Gnuplot command file
     std::ofstream gnuplot_commands(gnuplot_commands_path);
-    gnuplot_commands << "set terminal wxt size 1200,800\n";  // Set window size
-    gnuplot_commands << "set xlabel 'y_true'\n";              // Set x-axis label
-    gnuplot_commands << "set ylabel 'y_pred'\n";              // Set y-axis label
-    gnuplot_commands << "set title '" + plot_title + "'\n";   // Set plot title
-    gnuplot_commands << "set pointsize 1.5\n";                // Set point size
+    gnuplot_commands << "set terminal wxt size 1200,800\n";                             // Set window size
+    gnuplot_commands << "set xlabel 'y_true'\n";                                        // Set x-axis label
+    gnuplot_commands << "set ylabel 'y_pred'\n";                                        // Set y-axis label
+    gnuplot_commands << "set title '" + plot_title + "'\n";                             // Set plot title
+    gnuplot_commands << "set pointsize 1.5\n";                                          // Set point size
     gnuplot_commands << "plot 'temp/gnuplot.dat' using 1:2 with points pt 7 notitle\n"; // Plot y_true vs y_pred
 
     // Close the Gnuplot command file
@@ -99,12 +101,12 @@ void gnuplot_loss_plot(xt::xarray<double> loss_values, std::string plot_title)
 
     // Create the Gnuplot command file
     std::ofstream gnuplot_commands(gnuplot_commands_path);
-    gnuplot_commands << "set terminal wxt size 1200,800\n";         // Set window size
-    gnuplot_commands << "set xlabel 'Iteration'\n";                 // Set x-axis label
-    gnuplot_commands << "set ylabel 'Loss'\n";                      // Set y-axis label
-    gnuplot_commands << "set title '" + plot_title + "'\n";         // Set plot title
-    gnuplot_commands << "set grid\n";                               // Enable grid
-    gnuplot_commands << "set style line 1 lc rgb '#0060ad' lw 2\n"; // Define line style
+    gnuplot_commands << "set terminal wxt size 1200,800\n";                                         // Set window size
+    gnuplot_commands << "set xlabel 'Iteration'\n";                                                 // Set x-axis label
+    gnuplot_commands << "set ylabel 'Loss'\n";                                                      // Set y-axis label
+    gnuplot_commands << "set title '" + plot_title + "'\n";                                         // Set plot title
+    gnuplot_commands << "set grid\n";                                                               // Enable grid
+    gnuplot_commands << "set style line 1 lc rgb '#0060ad' lw 2\n";                                 // Define line style
     gnuplot_commands << "plot '" + data_path + "' using 1:2 with lines linestyle 1 title 'Loss'\n"; // Plot the data as a line
 
     gnuplot_commands.close();
@@ -113,27 +115,7 @@ void gnuplot_loss_plot(xt::xarray<double> loss_values, std::string plot_title)
     std::system(("gnuplot -persist " + gnuplot_commands_path).c_str());
 }
 
-    xt::xarray<double> create_random_dataset(float mean, float variance, int n_observations)
-    {
-        // Define seed
-        std::random_device rd;
-        std::mt19937 e2(rd());
-
-        std::normal_distribution<> dist(mean, variance);
-
-        // Init data array
-        xt::xarray<double> dataset = xt::zeros<double>({0, 2});
-        for (int n = 0; n < n_observations; n++)
-        {
-            xt::xarray<double> data_point = {dist(e2), dist(e2)};
-            data_point.reshape({1, 2});
-            dataset = xt::concatenate(xtuple(dataset, data_point), 0);
-        }
-
-        return dataset;
-    }
-
-void shuffleArray(xt::xarray<double> & array)
+void shuffleArray(xt::xarray<double> &array)
 {
     // Get the number of rows and columns
     std::size_t rows = array.shape(0);
@@ -161,12 +143,12 @@ void shuffleArray(xt::xarray<double> & array)
     array = temp;
 }
 
-    xarray<double> sigma(xarray<double> x)
-    {
-        return 1 / (1 + xt::exp(-x));
-    }
+xarray<double> sigma(xarray<double> x)
+{
+    return 1 / (1 + xt::exp(-x));
+}
 
-    xarray<double> sigma_derivative(xarray<double> x)
-    {
-        return sigma(x) * (1 - sigma(x));
-    }
+xarray<double> sigma_derivative(xarray<double> x)
+{
+    return sigma(x) * (1 - sigma(x));
+}

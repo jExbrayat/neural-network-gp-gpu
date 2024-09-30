62f16970e33d7c140f288e29b36c4319a2565bc4
jEx <jules.exbrayat@ensimag-inp.org>
2024-09-30 13:05:52 +0200

    parse a config file instead of arguments

--- main.cpp
+++ main.cpp
@@ -5,47 +5,58 @@
 #include <xtensor/xrandom.hpp>
 #include <xtensor-blas/xlinalg.hpp>
 #include <xtensor/xcsv.hpp>
+#include "definition.hpp"
+#include "backpropagation.cpp"
+#include <nlohmann/json.hpp> // Include the nlohmann/json library
 using namespace xt::placeholders;  // to enable _ syntax
 using namespace std;
 using namespace xt;
-#include "definition.hpp"
-#include "backpropagation.cpp"
+using json = nlohmann::json;
+
 
 void print_help(const std::string& program_name) {
-    std::cout << "Usage: " << program_name << " [options] <input_file.csv>\n"
-            << "Options:\n"
-            << "  --help                 Show this help message and exit\n"
-            << "  <input_file.csv>       Path to the input CSV file containing data\n\n"
-            << "Example:\n"
-            << "  " << program_name << " input_data.csv\n";
+    std::cout << "Usage: " << program_name << " config.json\n";
+    std::cout << "Pass a config.json file containing parameters.\n";
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
     // Check if the --help argument is passed
     if (argc > 1 && std::string(argv[1]) == "--help") {
         print_help(argv[0]);
         return 0;
     }
 
-    std::string file_name = argv[1];
-    std::string arg_epochs = argv[2];
-    std::string arg_learning_rate = argv[3];
-    std::string prediction_mode = argv[4];
-
-    int epochs = std::stoi(arg_epochs);    
-    double lr = std::stof(arg_learning_rate);    
-
-    // Open the file passed as argument
-    std::ifstream file(file_name);
-
-    if (!file.is_open()) {
-        std::cerr << "Error: Could not open file " << file_name << std::endl;
+    if (argc < 2) {
+        std::cerr << "Error: Missing config.json file path.\n";
+        print_help(argv[0]);
         return 1;
     }
 
+    std::string config_file = argv[1];
+    
+    // Read the JSON file
+    std::ifstream file(config_file);
+    if (!file.is_open()) {
+        std::cerr << "Error: Could not open config.json file.\n";
+        return 1;
+    }
+    json config;
+    file >> config;
+
+    // Parse the arguments from the JSON file
+    std::string dataset_path = config["dataset_path"];
+    int epochs = config["epochs"];
+    float learning_rate = config["learning_rate"];
+    std::string prediction_mode = config["prediction_mode"];
+    std::vector<int> network_architecture = config["network_architecture"];
+
     // Use xtensor-io to load the CSV data into an xtensor xarray
-    xt::xarray<double> dataset = load_csv<double>(file, ',');
+    std::ifstream dataset_file(dataset_path);
+    if (!dataset_file.is_open()) {
+        std::cerr << "Error: Could not open file " << dataset_path << std::endl;
+        return 1;
+    }
+    xt::xarray<double> dataset = load_csv<double>(dataset_file, ',');
     int input_csv_cols = dataset.shape(1);
     int x_dataset_cols = input_csv_cols - 1;
     
@@ -62,7 +73,7 @@ int main(int argc, char* argv[])
     xt::xarray<double> x_test = xt::view(dataset, xt::range(train_size, _), xt::range(0, input_csv_cols - 1));
     xt::xarray<double> y_test = xt::view(dataset, xt::range(train_size, _), xt::range(input_csv_cols - 1, input_csv_cols));; // shape (n, 1)
 
-    std::tuple weights_biases = make_gradient_descent(x_train, y_train, epochs, lr, vector<int> {10, 10, 1});
+    std::tuple weights_biases = make_gradient_descent(x_train, y_train, epochs, learning_rate, network_architecture);
 
     auto [weights, biases, mse_array] = weights_biases;
 

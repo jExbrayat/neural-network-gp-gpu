07681ee00e1ee3aac85148d88b655cf6d6e52b64
jEx <jules.exbrayat@ensimag-inp.org>
2024-11-19 20:47:58 +0100

    move json reading outside

--- src/include/utils.hpp
+++ src/include/utils.hpp
@@ -3,12 +3,14 @@
 
 #include <iostream>
 #include <xtensor/xarray.hpp>
+#include <nlohmann/json.hpp>
 using namespace std;
 using namespace xt;
 
 xarray<double> sigma(xarray<double> x);
 xarray<double> sigma_derivative(xarray<double> x);
 void print_shapes(xarray<double> &array, string msg);
+nlohmann::json read_json(const string &config_file_path);
 
 
 #endif // UTILS_HPP
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <xtensor/xarray.hpp>
+#include <nlohmann/json.hpp>
 using namespace std;
 using namespace xt;
 
@@ -16,4 +17,17 @@ xarray<double> sigma_derivative(xarray<double> x)
 void print_shapes(xarray<double> &array, string msg) {
     cout << msg << endl;
     cout << array.shape(0) << ", " << array.shape(1) << endl;
+}
+
+nlohmann::json read_json(const string &config_file_path) {
+    std::ifstream file(config_file_path);
+    if (!file.is_open())
+    {
+        std::cerr << "Error: Could not open config.json file.\n";
+        return 1;
+    }
+    nlohmann::json config;
+    file >> config;
+    file.close();
+    return config;
 }

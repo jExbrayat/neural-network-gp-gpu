f397daa8f9a485c9b69ebc84efe9731cbd798b77
jEx <jules.exbrayat@ensimag-inp.org>
2024-11-19 13:29:01 +0100

    feat: create train method with forward and backward pass

--- src/objects/gradient_descent.cpp
+++ src/objects/gradient_descent.cpp
@@ -17,7 +17,7 @@ public:
     GradientDescent(const xarray<double> &x_train, const xarray<double> &y_train, vector<xarray<double>> &weights, vector<xarray<double>> &biases);
     
     // Method
-    void train(const unsigned int &epochs, const unsigned int &batch_size, const float &learning_rate);
+    void train(const unsigned int &epochs, const int &batch_size, const float &learning_rate);
     
 protected:
     vector<xarray<double>> weights; // Define vector of tensors for making operations on weights
@@ -25,34 +25,96 @@ protected:
     vector<double> loss_history; // Store the loss over epochs
     xarray<double> x_train;
     xarray<double> y_train;
-    int num_layers;
 
 private:
     // Method
-    xarray<double> forward_pass(const xarray<double> &x_batch);
+    void forward_pass(const xarray<double> &x_batch);
+    void backward_pass(const xarray<double> &y_batch, const int &current_batch_size, const float &learning_rate);
+
+    // Class members
+    int num_layers;
+    std::vector<xarray<double>> layer_outputs;
+    std::vector<xarray<double>> layer_activations;
 };
 
 // Define constructor
 // Just init class members
 GradientDescent::GradientDescent(const xarray<double> &x_train, const xarray<double> &y_train, vector<xarray<double>> &weights, vector<xarray<double>> &biases) : x_train(x_train), y_train(y_train), weights(weights), biases(biases) {
     num_layers = weights.size(); 
+    layer_outputs.resize(num_layers);
+    layer_activations.resize(num_layers + 1);
 }
 
 
 // Write gradient descent methods
 
-xarray<double> GradientDescent::forward_pass(const xarray<double> &x_batch) {
+void GradientDescent::forward_pass(const xarray<double> &x_batch) {
 
-    std::vector<xarray<double>> layer_outputs(num_layers);
-    std::vector<xarray<double>> layer_activations(num_layers + 1);
     layer_activations[0] = xt::transpose(x_batch);
 
     for (int l = 0; l < num_layers; l++) {
         layer_outputs[l] = xt::linalg::dot(weights[l], layer_activations[l]) + biases[l];
         layer_activations[l + 1] = sigma(layer_outputs[l]); // sigma is defined in src/utils/utils.cpp
     }
+}
 
-    xarray<double>& last_activation = layer_activations[num_layers];
-    return last_activation;
+void GradientDescent::backward_pass(const xarray<double> &y_batch, const int &current_batch_size, const float &learning_rate) {
+    
+    vector<xarray<double>> deltas(num_layers);
+
+    // Init delta vector corresponding to the last layer
+    xarray<double> &last_activation = layer_activations[num_layers];
+    deltas[num_layers - 1] = (last_activation - xt::transpose(y_batch)) * sigma_derivative(layer_outputs[num_layers - 1]);
+
+    for (int l = num_layers - 2; l >= 0; l--) {
+        deltas[l] = xt::linalg::dot(xt::transpose(weights[l + 1]), deltas[l + 1]) * sigma_derivative(layer_outputs[l]);
+    }
+
+    // Update weights and biases
+    for (int l = 0; l < num_layers; l++) {
+        xarray<double> gradient_w = xt::linalg::dot(deltas[l], xt::transpose(layer_activations[l])) / current_batch_size; // Batch size may vary, at the end of epoch
+        xarray<double> gradient_b = xt::mean(deltas[l], {1});
+        gradient_b = gradient_b.reshape({gradient_b.size(), 1});
+
+        weights[l] -= learning_rate * gradient_w;
+        biases[l] -= learning_rate * gradient_b;
+    }
+}
+
+void GradientDescent::train(const unsigned int &epochs, const int &batch_size, const float &learning_rate) {
+    int dataset_size = x_train.shape()[0];
+    int batch_number = (dataset_size / batch_size);
+
+    for (unsigned int epoch = 0; epoch < epochs; epoch++) {
+
+        cout << "Epoch: " << epoch << endl;
+        float epoch_mse = 0;
+        int batch_id = 0;
+
+        for (int batch_start = 0; batch_start < dataset_size; batch_start += batch_size) {
+            // Plot currently processed batch number and increment
+            cout << "   Batch: " << batch_id << " / " << batch_number << endl;
+            batch_id++;
+
+            // Compute the current batch size, as defined normally but smaller if at the end of epoch
+            int current_batch_size = std::min(batch_size, dataset_size - batch_start);
+            xarray<double> x_batch = xt::view(x_train, range(batch_start, batch_start + current_batch_size), all());
+            xarray<double> y_batch = xt::view(y_train, range(batch_start, batch_start + current_batch_size), all());
+
+            // Perform the forward pass
+            xarray<double> forward_pass(x_batch); // Modify the layer_activations and layer_outputs
+            xarray<double> &last_activation = layer_activations[num_layers];
+
+            // Perform the backward pass
+            backward_pass(y_batch,  current_batch_size, learning_rate); // Modify the weights and biases
+ 
+            // Compute the loss for the current batch
+            xarray<double> squared_error = xt::pow(last_activation - xt::transpose(y_batch), 2); // Error for each pixel of each observation
+            xarray<double> observation_mse = xt::mean(squared_error, {0}); // Mean over all the pixels in the observations
+            epoch_mse += xt::sum(observation_mse)() / dataset_size;
+        }
+        cout << "Epoch " << epoch << " MSE: " << epoch_mse << endl;
+        loss_history.push_back(epoch_mse);
+    }
 }
 
